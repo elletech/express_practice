c003f45fd3ad84b87d163b044d09d44c1056a838
elletech <mygit_yutasugao20171126@yahoo.co.jp>
2021-10-31 02:32:45 +0000

    partials helper

--- server.js
+++ server.js
@@ -2,23 +2,24 @@ const express = require("express");
 const app = express();
 const hbs = require("hbs");
 app.set("view engine", "hbs");
-
+hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
 // ミドルウェア(middleware)
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.render("home.hbs", {
     pageTitle: "Home Page",
-    content: "当ホームページへようこそ！",
-    currentYear: new Date().getFullYear()
+    content: "当ホームページへようこそ！"
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about.hbs", {
     pageTitle: "About Page",
-    content: "コンテンツです。",
-    currentYear: new Date().getFullYear()
+    content: "コンテンツです。"
   });
 });
 
